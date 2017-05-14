fe64871da3bdb4d666174931ad1bbc349baef00e
whalesharkking <elias_christen@gmx.ch>
2017-05-14 13:40:52 +0200

    funktionierende Version

--- app/src/main/java/com/example/elias/sagetext/MainActivity.java
+++ app/src/main/java/com/example/elias/sagetext/MainActivity.java
@@ -4,32 +4,75 @@ import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener, View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener, View.OnClickListener, AdapterView.OnItemSelectedListener {
 
-        public TextToSpeech tts;
-        @Override
+private TextToSpeech tts;
+    private Spinner spinner;
+    private static final String[]paths = {"Deutsch", "Englisch", "Französisch", "China!!!"};
+
+
+
+
+    @Override
         protected void onCreate (Bundle savedInstanceState){
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tts= new TextToSpeech(this,this);
+        spinner = (Spinner)findViewById(R.id.spinner);
+        ArrayAdapter<String>adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_spinner_item,paths);
 
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
 
     }
     public void onClick(View v){
         EditText editText= (EditText) findViewById(R.id.editText);
-        tts.speak(editText.getText().toString(),TextToSpeech.QUEUE_FLUSH,null);
+
+       tts.speak(editText.getText().toString(),TextToSpeech.QUEUE_FLUSH,null);
 
     }
 
     @Override
     public void onInit(int status) {
-        tts.setLanguage(Locale.GERMAN);
+
+
 
         Button button= (Button) findViewById(R.id.button);
         button.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch (position) {
+            case 0:
+            tts.setLanguage(Locale.GERMAN);
+                break;
+            case 1:
+             tts.setLanguage(Locale.ENGLISH);
+                break;
+            case 2:
+              tts.setLanguage(Locale.FRENCH);
+                break;
+            case 3:
+                tts.setLanguage(Locale.CHINA);
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
